91ffa09983cd38c906020881d2f05957cd018884
Yaroslav <legionerov0310@gmail.com>
2026-04-22 19:28:08 +0300

    4.5.rasp Sinus

---
+++ get-dac/r2r-sin.py
@@ -0,0 +1,25 @@
+import r2r_dac as r2r
+import signal_generator as sg
+import time
+
+
+amplitude = 3.2
+signal_frequency = 10
+sampling_frequency = 1000
+
+
+if __name__ == "__main__":
+    dac = None
+    try:
+        dac = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, False)
+        start_time = time.time()
+
+        while True:
+            current_time = time.time() - start_time
+            voltage = amplitude * sg.get_sin_wave_amplitude(signal_frequency, current_time)
+            dac.set_voltage(voltage)
+            sg.wait_for_sampling_period(sampling_frequency)
+
+    finally:
+        if dac is not None:
+            dac.deinit()
